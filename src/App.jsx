2fceb85b2bde58adeb985746de0235824adc8e75
unknown <10288694@student.walsallcollege.ac.uk>
2025-02-13 23:31:30 +0000

    added a favorites page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import Text from './components/Text';
 import MovieCard from './components/MovieCard';
+import { Route, Routes } from 'react-router-dom';
+import Favorites from './pages/Favorites';
 
 function App() {
   return (
     <>
+    <main className='main-content'>
+      <Routes>
+        <Route path = "/favorites" element = {<Favorites />}/>
+        </Routes>
       <h1><Text property={"Movies"} /></h1>
-      
       <MovieCard />
+      </main>
     </>
   );
 }
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,14 @@
+const Favorites = () => {
+    return(
+        <>
+        <div className="favorites-empty">
+            <h1>Favorites</h1>
+            <p>Start adding favorites!</p>
+        </div>
+        </>
+    )
+}
+
+
+
+export default Favorites; // Exports the Favorite component to be used in other files.
